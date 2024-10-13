0b85487e5f9b45c79d96beeee757cb19ebd8ac32
Wlad <136496464+VladTer06081963@users.noreply.github.com>
2024-10-13 19:47:35 +0000

    add pug

--- bot.js
+++ bot.js
@@ -1,8 +1,19 @@
+const fs = require('fs');
+
+const pug = require('pug');
+const html = pug.renderFile('layout.pug');
+fs.writeFileSync('index.html', html);
+
+
 const TelegramBot = require('node-telegram-bot-api');
-const dotenv = require('dotenv');
+require('dotenv').config();
+
+
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.token;
+const webAppUrl = 'https://vladter06081963.github.io/onlineLesson_1/'
+
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -11,9 +22,22 @@ const bot = new TelegramBot(token, {polling: true});
 
 // Listen for any kind of message. There are different kinds of
 // messages.
-bot.on('message', (msg) => {
+bot.on('message',async (msg) => {
   const chatId = msg.chat.id;
+  const text = msg.text;
 
+
+  if(text === '/start') {
+    await bot.sendMessage(chatId, 'Ниже появится кнопка, Заполни форму,', {
+      reply_markup: {
+        inline_keyboard: [
+          [{text: 'заполни форму', web_app: {url: webAppUrl}}]
+        ]
+      }
+    }
+
+    )
+  }
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  await bot.sendMessage(chatId, 'Received your message ' + `${chatId}`);
 });
